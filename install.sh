b6ad86b5f278eda4b72bbaf593eb8388dbd0f779
ryoung141 <ryoung141@gmail.com>
2017-06-10 14:54:42 -0700

    added symlink automation to install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle/ && git clone --recursive https://github.com/davidhalter/jedi-vim.git
+ln -sf ~/vimrc/vimrc ~/.vimrc
